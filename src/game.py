ed723386bfe49ee26f8b4bc5a015eda9b46f82b1
Brad Gatewood <gatewoodbt@outlook.com>
2023-05-19 02:26:56 -0400

    Update asset pipeline

--- src/game.py
+++ src/game.py
@@ -5,8 +5,6 @@ from player import Player
 from weapon import Weapon
 
 class Game:
-    # TODO:  Draw and move background.
-
     def __init__(self):
         self.background = pygame.image.load('data/background.png').convert()
 
@@ -20,23 +18,24 @@ class Game:
         self.player.update(self.weapon)
         self.weapon.update()
     
-    def render(self, screen):
-        # TODO: Study. This. Code!  What IS the offset value?!
-        # NOTE: The offset is the player's distance from the screen's center.
+    def render(self, screen):  # TODO: Study. This. Code!
+        # get the player's current distance from the screen's center
         offset = pygame.math.Vector2()
         offset.x = self.player.rect.centerx - SCREEN_CENTER[0]
         offset.y = self.player.rect.centery - SCREEN_CENTER[1]
 
-        background_offset_pos = self.background.get_rect().topleft - offset
-        screen.blit(self.background, background_offset_pos)
+        # subtract offset from each sprite's position to get render pos
+        offset_pos = self.background.get_rect().topleft - offset
+        screen.blit(self.background, offset_pos)
 
         offset_pos = self.player.rect.topleft - offset
         self.player.render(screen, offset_pos)
+
         offset_pos = self.weapon.rot_rect.topleft - offset
         self.weapon.render(screen, offset_pos)
 
-        # NOTE: Tints crosshair. Is this how to tint the base white textures?
+        # tint crosshair  # TODO: Use this to tint the base white textures?
         surf = self.cursor.copy()
         surf.fill((0, 0, 0, 255), None, pygame.BLEND_RGBA_MULT)
-        # surf.fill((255, 0, 0, 0), None, pygame.BLEND_RGBA_ADD)  # tint red
+        surf.fill((255, 0, 0, 0), None, pygame.BLEND_RGBA_ADD)  # tint red
         screen.blit(surf, self.cursor.get_rect(topleft=pygame.mouse.get_pos()))
