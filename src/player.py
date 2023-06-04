9b24e4909ce56d38458c10f6e273ad5778fcd51a
Brad Gatewood <gatewoodbt@outlook.com>
2023-06-04 03:42:13 -0400

    Load and render animations from json data

--- src/player.py
+++ src/player.py
@@ -11,33 +11,17 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        # TODO: Load animations from json data.
-        self.animations = load_animation_data()
-
-        NUM_WALK_FRAMES = 8
-        spritesheet = pygame.image.load('data/character_anim.png').convert_alpha()
-        self.frames = []
-        for i in range(NUM_WALK_FRAMES):
-            frame = pygame.Surface((96,64))
-            frame.blit(spritesheet, (0,0), (96 * i, 0, 96, 64))
-            frame = pygame.transform.scale_by(frame, SCALE_FACTOR)
-            self.frames.append(frame)
-        
-        # NUM_IDLE_FRAMES = 9
-        # self.frames: pygame.Surface = []
-        # for i in range(NUM_IDLE_FRAMES):
-        #     frame = pygame.Surface((96,64))
-        #     frame.blit(spritesheet, (0,0), (96 * i, 64 * 2, 96, 64))  # NOTE: y += 1
-        #     frame = pygame.transform.scale_by(frame, SCALE_FACTOR)
-        #     self.frames.append(frame)
-        
+        self.animations = load_animation_data()  # TODO: Create a dict of anims.
+       
         # setup anim data
         self.frame_timer = 0
         self.frame_duration = 75  # milliseconds
         self.current_frame = 0
 
+        self.current_animation = self.animations['attack']
+
         # setup sprite data
-        self.image = self.frames[self.current_frame]
+        self.image = self.current_animation[self.current_frame]
         self.rect = self.image.get_rect(center = SCREEN_CENTER)
 
         # TODO: Draw image and hitbox rects for testing.
@@ -46,7 +30,6 @@ class Player(pygame.sprite.Sprite):
         self.frame_timer += delta_time
         if self.frame_timer >= self.frame_duration:
             self.frame_timer -= self.frame_duration
-            self.current_frame += 1
-        if self.current_frame >= len(self.frames):
+        if self.current_frame == len(self.current_animation):
             self.current_frame = 0
-        self.image = self.frames[self.current_frame]
+        self.image = self.current_animation[self.current_frame]
--- src/resources.py
+++ src/resources.py
@@ -4,36 +4,22 @@ import pygame
 
 SCALE_FACTOR = 3  # 16x16 -> 48x48  (96x64 * 3 for character sprites)
 
-class Animation:
-    def __init__(self, name):
-        self.name = name
-        self.frames = []
-    
-    def add_frame(self, frame):
-        self.frames.append(frame)
-
-
 def load_animation_data():
     spritesheet = pygame.image.load('data/character_anim.png').convert_alpha()
-    animations = []
+    anim_dict = {}  # keys = anim name, values = lists of frames
 
     with open('data/character_anim.json') as infile:
         data = json.load(infile)
-        animation = None
         for key in data['frames'].keys():
-            anim_name = ''
-            frame_num = ''
-            for ch in key:
-                if not ch.isdigit():
-                    anim_name += ch
-                else:
-                    frame_num += ch
+            anim_name = ''  # key for anim_dict
 
-            if animation == None:
-                animation = Animation(anim_name)
-            elif animation.name != anim_name:
-                animations.append(animation)
-                animation = Animation(anim_name)
+            i = 0
+            while key[i].isalpha():
+                anim_name += key[i]
+                i += 1
+
+            if anim_name not in anim_dict:
+                anim_dict[anim_name] = []
             
             frame_size = (data['frames'][key]['sourceSize']['w'],
                           data['frames'][key]['sourceSize']['h'])
@@ -43,7 +29,9 @@ def load_animation_data():
                     frame_rect['w'], frame_rect['h'])
             frame.blit(spritesheet, (0,0), area)
             frame = pygame.transform.scale_by(frame, SCALE_FACTOR)
-            animation.add_frame(frame)
+            anim_dict[anim_name].append(frame)
 
-    for anim in animations:
-        print(anim.name + ', ' + str(len(anim.frames)))
+    for k, v in anim_dict.items():
+        print(k + ', ' + str(len(v)))
+    
+    return anim_dict
