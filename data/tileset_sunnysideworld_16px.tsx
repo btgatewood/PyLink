d0e78fed35907c7aaa7517911a1e4cb354f8090a
Brad Gatewood <gatewoodbt@outlook.com>
2023-06-06 04:51:00 -0400

    More tilemap development

--- data/tileset_sunnysideworld_16px.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tileset_sunnysideworld_16px" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="tileset_sunnysideworld_16px.png" width="1024" height="1024"/>
-</tileset>
