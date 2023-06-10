8ad5280af6a0431e603972cc06eaefaf92666342
Brad Gatewood <gatewoodbt@outlook.com>
2023-06-10 03:57:49 -0400

    Add transparent background to console

--- src/debug.py
+++ src/debug.py
@@ -31,17 +31,24 @@ class Console:
         for line in self.messages:
             text += line + '\n'
 
-        self.surf = self.font.render(text, antialias, color, bgcolor)
-        self.rect = self.surf.get_rect(
-            bottomleft = (10, 720 - 10)  # TODO: use screen height
-        )
-    
+        self.surf = self.font.render(text, antialias, color)
+        
+        # create transparent, inflated background
+        self.bg_rect = self.surf.get_rect()
+        self.bg_rect.inflate_ip(16, 16)
+        self.bg_surf = pygame.surface.Surface(self.bg_rect.size).convert_alpha()
+        self.bg_surf.fill((0, 0, 0, 128))
+        self.bg_rect.bottomleft = (16, SCREEN_HEIGHT - 16)
+
+        self.rect = self.surf.get_rect(center = self.bg_rect.center)
+
     def set_fps_text(self, text):
-        self.fps_surf = self.font.render(text, antialias, color, bgcolor)
+        self.fps_surf = self.font.render(text, antialias, color)
         self.fps_rect = self.fps_surf.get_rect(
             topright = (1280 - 10, 10)  # TODO: use screen width 
         )
 
     def render(self, screen):
+        screen.blit(self.bg_surf, self.bg_rect)
         screen.blit(self.surf, self.rect)
         screen.blit(self.fps_surf, self.fps_rect)
